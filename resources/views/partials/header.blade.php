f622a9d5761ed7230ceba3cc2dee0c14d1ef04d9
FR_Jess <fr_jess@icloud.com>
2022-12-22 15:26:25 +0100

    [add] logo header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,5 @@
 <header>
-    header
+    <div class="logo">
+        <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="Logo DC">
+    </div>
 </header>
---
+++ resources/views/single.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('content')
+    <main>
+        comics details
+    </main>
+@endsection
+
+@section('title')
+    Comics details
+@endsection
