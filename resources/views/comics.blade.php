494a44ce845d77d9ad44a32813aa546ff424ac7f
Jessica <fr_jess@icloud.com>
2023-03-16 14:13:09 +0100

    [fix] card h4 position

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.main')
 
 @section('content')
-  <main>
-    <h2 class>Current series</h2>
-    <div class="content">
-      @foreach ($comics as $comic)
-        <div class="card">
-          <a href="{{ route('comic_detail', ['id' => $comic['id']]) }}" class="card">
-            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
-            <h4>{{ $comic['title'] }}</h4>
-          </a>
-        </div>
-      @endforeach
+ <main>
+  <h2 class>Current series</h2>
+  <div class="content">
+   @foreach ($comics as $comic)
+    <div class="card">
+     <a href="{{ route('comic_detail', ['id' => $comic['id']]) }}">
+      <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+      <h4>{{ $comic['title'] }}</h4>
+     </a>
     </div>
-    <button>Load more</button>
-  </main>
+   @endforeach
+  </div>
+  <button>Load more</button>
+ </main>
 @endsection
 
 @section('title')
-  comics
+ comics
 @endsection
