cb9890afb1ca3327ec8d91a4885e6fb368feda85
Jessica <fr_jess@icloud.com>
2023-03-16 14:22:34 +0100

    [edit] detail card

--- resources/views/comic_detail.blade.php
+++ resources/views/comic_detail.blade.php
@@ -1,25 +1,25 @@
 @extends('layouts.main')
 
 @section('content')
-  <main>
-    <div class="container">
+ <main>
+  <div class="container">
 
-      <div class="card detail">
-        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
-        <h1>{{ $comic['title'] }}</h1>
-        <h5>Series: {{ $comic['series'] }} | Release date: {{ $comic['sale_date'] }} | Price: {{ $comic['price'] }}</h5>
-        <h4>Art by: {{ implode(', ', $comic['artists']) }} </h4>
-        <h4>Written by: {{ implode(', ', $comic['writers']) }} </h4>
-        <p>
-          {!! $comic['description'] !!}
-        </p>
-      </div>
+   <div class="card-detail">
+    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+    <h1>{{ $comic['title'] }}</h1>
+    <h5>Series: {{ $comic['series'] }} | Release date: {{ $comic['sale_date'] }} | Price: {{ $comic['price'] }}</h5>
+    <h4>Art by: {{ implode(', ', $comic['artists']) }} </h4>
+    <h4>Written by: {{ implode(', ', $comic['writers']) }} </h4>
+    <p>
+     {!! $comic['description'] !!}
+    </p>
+   </div>
 
 
-    </div>
-  </main>
+  </div>
+ </main>
 @endsection
 
 @section('title')
-  Comics
+ Comics
 @endsection
